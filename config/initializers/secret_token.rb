80f57448be42223d950079ea58688576d6ecc69a
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-09-30 14:24:44 +0100

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schoolmaster::Application.config.secret_token = '0efb7bb40bdeec9651a0873c6fd66971ac535aa31817c0c7cb39861aa6c5ba1103d85b443b2458cbc1969ac9cd7831296f1ae879c447ee400fecadb0ec304dc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Schoolmaster::Application.config.session_store :cookie_store, :key => '_schoolmaster_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Schoolmaster::Application.config.session_store :active_record_store
