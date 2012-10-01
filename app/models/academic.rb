108bd609389c095c10b9ea0c0736a7acb1cffe64
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-10-01 20:25:18 +0100

    modified index page.

--- app/models/academic.rb
+++ app/models/academic.rb
@@ -1,3 +1,19 @@
 class Academic < ActiveRecord::Base
   attr_accessible :first, :home_url, :image_url, :last, :title, :work_mail, :work_phone
+
+  def name
+    "#{title} #{first} #{last}"
+  end
+
+  def to_s
+    name
+  end
+
+  def email
+    "#{work_mail}.nuigalway.ie"
+  end
+
+  def mailto
+    "mailto:#{email}"
+  end
 end
