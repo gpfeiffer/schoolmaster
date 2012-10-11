09e5982de7859eafc7d71381deb147a15124a510
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-10-12 00:57:18 +0100

    modified student views.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,11 @@
 class Student < ActiveRecord::Base
   attr_accessible :completed_on, :cosupervisor, :email, :expected_on, :first, :funder, :home_url, :interests, :last, :location, :photo_url, :programme, :registration, :started_on, :status, :supervisor, :title, :work_phone
+
+  def name
+    "#{first} #{last}"
+  end
+
+  def to_s
+    name
+  end
 end
