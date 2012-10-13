ba94c87a0e68f134f487dcea16635b3a39e0af3c
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-10-13 14:18:07 +0100

    added home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Schoolmaster::Application.routes.draw do
+  get "home/index"
+
   resources :students
 
   resources :academics
@@ -53,6 +55,7 @@ Schoolmaster::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
