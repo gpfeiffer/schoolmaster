7fe1aeef56eab8163ac0f77b530f70f75bd1e5e0
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-10-14 14:26:28 +0100

    modified student views.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,24 @@
 ##  this file contains the definitions of constants used in the application.
 
+# academic titles
 TITLES = [
   'Dr',
   'Mr',
   'Ms',
   'Prof.',
+]
+
+# postgraduate programmes
+PROGRAMMES = [
+  "MSc",
+  "PhD", 
+]
+
+# student status
+STATUSES = [
+  "Full time", 
+  "Part time", 
+  "Incomplete", 
+  "Completed", 
+  "On leave",
 ]
