e644ecd257737adf1c6ab3ed5f56f80ec6f07a8f
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-11-17 21:20:25 +0000

    installed devise and user model.

--- db/migrate/20121116192703_devise_create_users.rb
+++ db/migrate/20121116192703_devise_create_users.rb
@@ -2,7 +2,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :name,               :null => false, :default => ""
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
@@ -38,7 +37,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :name,                 :unique => true
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
