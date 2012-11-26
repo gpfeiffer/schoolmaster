21c62f5818df1ee241fb4cea7063a9a245e9325b
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-11-26 12:35:34 +0000

    mail setup.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,17 @@ Schoolmaster::Application.configure do
  
   # set default url options for devise 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # mailer settings.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address  => "smtp.gmail.com",
+    :port  => 587,
+    :domain  => "nuigalway.ie",
+    :authentication => "plain",
+    :user_name  => "cs424.nuig",
+    :password  => "........",
+    :enable_starttls_auto => true
+  }
 end
