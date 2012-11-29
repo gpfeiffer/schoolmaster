a2f343a8d99fd7a5c773269c4653796147fe28a8
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-11-29 13:05:54 +0000

    fixed (?) symbols.
    Some generator wrote json: @records,
    but that might be a ruby 1.9 shorthand ...

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,7 +6,7 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @records }
+      format.json { render :json =>  @records }
     end
   end
 
@@ -17,7 +17,7 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @record }
+      format.json { render :json =>  @record }
     end
   end
 
@@ -29,7 +29,7 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @record }
+      format.json { render :json =>  @record }
     end
   end
 
@@ -45,11 +45,11 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       if @record.save
-        format.html { redirect_to @record.academic, notice: 'Record was successfully created.' }
-        format.json { render json: @record, status: :created, location: @record }
+        format.html { redirect_to @record.academic, :notice =>  'Record was successfully created.' }
+        format.json { render :json =>  @record, :status =>  :created, :location => @record }
       else
-        format.html { render action: "new" }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+        format.html { render :action =>  "new" }
+        format.json { render :json =>  @record.errors, :status =>  :unprocessable_entity }
       end
     end
   end
@@ -61,11 +61,11 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       if @record.update_attributes(params[:record])
-        format.html { redirect_to @record.academic, notice: 'Record was successfully updated.' }
+        format.html { redirect_to @record.academic, :notice =>  'Record was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+        format.html { render :action =>  "edit" }
+        format.json { render :json =>  @record.errors, :status =>  :unprocessable_entity }
       end
     end
   end
