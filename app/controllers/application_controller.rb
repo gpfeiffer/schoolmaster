2616cfe41046cdb6bd88ecec690d412fc3b606ec
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-12-03 09:54:15 +0000

    redirect academic to their page after login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "Access denied."
     redirect_to root_url
   end
+
+  #  test.  redirect after login.
+  def after_sign_in_path_for(user)
+    if user.academic
+      academic_path(user.academic)
+    else
+      root_path
+    end
+  end
 end
