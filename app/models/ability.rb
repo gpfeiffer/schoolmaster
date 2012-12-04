c6becbbd9e43b567cfd90f48154b294dfef02730
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2012-12-04 13:46:11 +0000

    supervisor can update GRC.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,25 @@ class Ability
       can :manage, :all
     elsif user.role? :supervisor
       can :read, [Academic, Student, Room]
-    end
+      can :update, Student do |student|
+        student.academics.include? user.academic
+      end
+
+      # FIXME: can only create committee members for own students.
+      can :create, Committee 
+
+      can :destroy, Committee do |committee|
+        committee.student.academics.include? user.academic
+      end
+ 
+      # FIXME: can only create supervision members for own students.
+      can :create, Supervision 
+
+      can :destroy, Supervision do |supervision|
+        supervision.student.academics.include? user.academic
+      end
+    else
+      can :read [Academic, Student]
+   end
   end
 end
