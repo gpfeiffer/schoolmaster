89519e5fdced11f067bab13c6e091519843f25cc
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-02-09 19:59:55 +0000

    added dependencies

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@ class Project < ActiveRecord::Base
   attr_accessible :author_id, :title
 
   belongs_to :author
-  has_one :direction
+  has_one :direction, :dependent => :destroy
   has_one :supervisor, :through => :direction, :source => :academic
-  has_many :examinations
+  has_many :examinations, :dependent => :destroy
 
   def supervisor_id
     supervisor.id if supervisor
