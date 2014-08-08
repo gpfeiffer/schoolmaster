a28c2ec0e3658e602192ed380cc3e50b2752d8c5
Goetz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-08 10:57:09 +0100

    more instances

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -4,6 +4,6 @@ class Instance < ActiveRecord::Base
   belongs_to :atom
   belongs_to :course
 
-  COHORTS = %w{ 1BMS1 1BS1 2BMS1 }
+  COHORTS = %w{ 1BMS1 1BS1 1FM1 2BMS1 2FM1 3BMS2 3FM2 4BMS2 4FM2 }
   STATUSES = %w{ C O }
 end
