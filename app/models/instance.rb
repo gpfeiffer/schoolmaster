953b90868eac0c319b5b211ebb44b5f6f0bfe6c0
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-10 19:26:53 +0100

    add molecule_id to instance

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,7 +1,8 @@
 class Instance < ActiveRecord::Base
-  attr_accessible :atom_id, :code, :cohort, :course_id, :status
+  attr_accessible :atom_id, :code, :cohort, :course_id, :molecule_id, :status
 
   belongs_to :atom
+  belongs_to :molecule
   belongs_to :course
 
   COHORTS = %w{ 1BMS1 1BS1 1FM1 2BMS1 2FM1 3BMS2 3FM2 4BMS2 4FM2 }
