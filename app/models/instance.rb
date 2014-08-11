80ee1fbd160653ad354b7eef4bce91dd6a9c8ad1
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-11 17:11:22 +0100

    delegate to molecule

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,12 +1,12 @@
 class Instance < ActiveRecord::Base
-  attr_accessible :atom_id, :code, :cohort, :course_id, :date, :molecule_id, :status
+  attr_accessible :cohort, :date, :molecule_id, :status
 
-  belongs_to :atom
   belongs_to :molecule
-  belongs_to :course
 
   COHORTS = %w{ 1BMS1 2BMS1 3BMS2 4BMS2 1BS1 1FM1 2FM1 3FM2 4FM2 }
   STATUSES = %w{ C O }
+
+  delegate :code, :to => :molecule
 
   def loads
     molecule.loads.group_by(&:date)[date] || []
