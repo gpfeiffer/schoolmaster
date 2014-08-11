9a9bb5d37d309abf696773d609628ea33b131850
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-11 19:18:19 +0100

    added cohorts

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -3,7 +3,7 @@ class Instance < ActiveRecord::Base
 
   belongs_to :molecule
 
-  COHORTS = %w{ 1BMS1 2BMS1 3BMS2 4BMS2 1BS1 1FM1 2FM1 3FM2 4FM2 }
+  COHORTS = %w{ 1BME1 2BME1 3BME1 4BME1 1BMS1 2BMS1 3BMS2 4BMS2 1BS1 1FM1 2FM1 3FM2 4FM2 }
   STATUSES = %w{ C O }
 
   delegate :code, :course, :to => :molecule
