4c720eefd2bd630e155c3f05125437b0f45e0cac
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2014-08-26 17:47:52 +0100

    remove stream from instance

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,27 +1,13 @@
 class Instance < ActiveRecord::Base
-  attr_accessible :stream, :cohort_id, :credits, :date, :molecule_id, :status
+  attr_accessible :cohort_id, :credits, :date, :molecule_id, :status
 
   belongs_to :molecule
   belongs_to :cohort
 
-  STREAMS = %w{ 
-    1BMS1 2BMS1 3BMS2 4BMS2 
-    1BS1 2BS1 3BS9 4BS3 
-    1FM1 2FM1 3FM2 4FM2
-    1BME1 2BME1 3BME1 4BME1 
-    1BA1 2BA1-MA2030 2BA1-MH2030 3BA1-MA3030 3BA1-MH3030
-    1BCx 2BCx
-    1BCT1 2BCT1 3BCT1 4BCT1 
-    1Eng 2Eng
-    1BY1 
-    1BCM1 
-    CS201 CS322
-  }
   STATUSES = %w{ C O }
 
   delegate :code, :course, :to => :molecule
 
-  validates :stream, :inclusion => STREAMS
   validates :status, :inclusion => STATUSES
 
   def loads
